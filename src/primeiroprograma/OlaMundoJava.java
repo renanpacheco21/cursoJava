d2a06b90c35d9f5ac42056082fd37c29b8c98ca1
renanpacheco21 <renan1025>
2022-06-14 22:38:41 -0300

    RenanCommit

--- src/primeiroprograma/OlaMundoJava.java
+++ src/primeiroprograma/OlaMundoJava.java
@@ -1,4 +1,22 @@
 package primeiroprograma;
 
-public class OlaMundoJava {
+import javax.swing.*;
+
+public class OlaMundoJava extends JDialog {
+    private JPanel contentPane;
+    private JButton button1;
+    private JButton buttonOK;
+
+    public OlaMundoJava() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+    }
+
+    public static void main(String[] args) {
+        OlaMundoJava dialog = new OlaMundoJava();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
 }
