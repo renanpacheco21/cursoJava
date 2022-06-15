e6c844795c1554a1d7ace4d126c55d46c76c567d
renanpacheco21 <renan1025>
2022-06-15 09:41:17 -0300

    RenanCommit

--- src/primeiroprograma/OlaMundoJava.java
+++ src/primeiroprograma/OlaMundoJava.java
@@ -1,16 +1,25 @@
 package primeiroprograma;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OlaMundoJava extends JDialog {
     private JPanel contentPane;
     private JButton button1;
+    private JLabel olaMundo;
     private JButton buttonOK;
 
     public OlaMundoJava() {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                olaMundo.setText("Olá mundo!!!");
+            }
+        });
     }
 
     public static void main(String[] args) {
