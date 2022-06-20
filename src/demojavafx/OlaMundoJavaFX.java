55cd4abd63b554a87a9bfe7cda5097b9f2a958c1
renanpacheco21 <renan1025>
2022-06-20 20:16:28 -0300

    RenanCommit

---
+++ src/demojavafx/OlaMundoJavaFX.java
@@ -0,0 +1,16 @@
+package demojavafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OlaMundoJavaFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
