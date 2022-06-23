2d392ac2c6c411d226d3d0a53814d1f3efb41529
renanpacheco21 <renan1025>
2022-06-23 09:21:49 -0300

    RenanCommit

--- src/horadosistema/HoraDoSistema.java
+++ src/horadosistema/HoraDoSistema.java
@@ -1,5 +1,8 @@
 package horadosistema;
+import javax.sound.midi.Soundbank;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class HoraDoSistema {
 
@@ -8,5 +11,16 @@ public class HoraDoSistema {
         System.out.println("A hora do sistema é");
         System.out.println(relogio.toString());
 
+        Scanner dt = new Scanner(System.in);
+        System.out.print("Digite o ano de nascimento: ");
+        int ano = dt.nextInt();
+
+        Calendar calendario = Calendar.getInstance();
+        //System.out.println(calendario.get(Calendar.YEAR));
+
+        int idade = calendario.get(Calendar.YEAR) - ano;
+        System.out.println("Você tem "+ idade + " anos");
+
     }
+
 }
