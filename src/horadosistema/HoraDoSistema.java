5136529baa784c4ea26ce62914fe4a8fa52693fa
renanpacheco21 <renan1025>
2022-06-27 15:34:23 -0300

    RenanCommit

--- src/horadosistema/HoraDoSistema.java
+++ src/horadosistema/HoraDoSistema.java
@@ -2,7 +2,6 @@ package horadosistema;
 import javax.swing.*;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Scanner;
 
 public class HoraDoSistema {
 
@@ -11,14 +10,23 @@ public class HoraDoSistema {
         System.out.println("A hora do sistema é");
         System.out.println(relogio.toString());
 
-        Scanner dt = new Scanner(System.in);
+        //Scanner dt = new Scanner(System.in);
         //System.out.print("Digite o ano de nascimento: ");
         //int ano = dt.nextInt();
         String anoNasc = JOptionPane.showInputDialog("Digite o ano de nascimento: ");
+
         Calendar calendario = Calendar.getInstance();
         Integer idade = calendario.get(Calendar.YEAR) - Integer.parseInt(anoNasc);
 
+        if(Integer.parseInt(anoNasc) > 1 || Integer.parseInt(anoNasc) < 2023){
+            JOptionPane.showMessageDialog(null,"Você tem "+ idade + " anos");
+        }else {
+            JOptionPane.showMessageDialog(null, "O valor informado não é válido");
+        }
+
+
+
         //System.out.println("Você tem "+ idade + " anos");
-        JOptionPane.showMessageDialog(null,"Você tem "+ idade + " anos");
+        //JOptionPane.showMessageDialog(null,"Você tem "+ idade + " anos");
     }
 }
