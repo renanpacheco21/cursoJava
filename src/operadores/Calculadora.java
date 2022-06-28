d1c94820e59aceeaaa0a8190127c2b29086522df
renanpacheco21 <renan1025>
2022-06-27 22:17:17 -0300

    RenanCommit

--- src/operadores/Calculadora.java
+++ src/operadores/Calculadora.java
@@ -6,11 +6,10 @@ public class Calculadora {
 
     public static void main(String[] args) {
 
-        //variaveis
-        int x = 0;
-        int n1 = 0;
-        int n2 = 0;
-        String operacao = "";
+        int calc;
+        int n1;
+        int n2;
+        String operacao;
 
         n1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:"));
         operacao = JOptionPane.showInputDialog("Escolha a operação (+) (-) (*) (/) :");
@@ -18,20 +17,20 @@ public class Calculadora {
 
         switch (operacao){
             case "+":
-                x = n1 + n2;
-                JOptionPane.showMessageDialog(null,"O Resultado da adição é " + x);
+                calc = n1 + n2;
+                JOptionPane.showMessageDialog(null,"O Resultado da adição é " + calc);
                 break;
             case "-":
-                x = n1 - n2;
-                JOptionPane.showMessageDialog(null,"O Resultado da subtração é " + x);
+                calc = n1 - n2;
+                JOptionPane.showMessageDialog(null,"O Resultado da subtração é " + calc);
                 break;
             case "*":
-                x = n1 * n2;
-                JOptionPane.showMessageDialog(null,"O Resultado multiplicação é " + x);
+                calc = n1 * n2;
+                JOptionPane.showMessageDialog(null,"O Resultado multiplicação é " + calc);
                 break;
             case "/":
-                x = n1 / n2;
-                JOptionPane.showMessageDialog(null,"O Resultado da divisão é " + x);
+                calc = n1 / n2;
+                JOptionPane.showMessageDialog(null,"O Resultado da divisão é " + calc);
                 break;
             default:
                 JOptionPane.showMessageDialog(null,"A operação não é válida");
