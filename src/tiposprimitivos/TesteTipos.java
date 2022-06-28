167505d49736e0a9a9f20647a4715a12ac10a56b
renanpacheco21 <renan1025>
2022-06-27 22:28:15 -0300

    RenanCommit

--- src/tiposprimitivos/TesteTipos.java
+++ src/tiposprimitivos/TesteTipos.java
@@ -5,14 +5,13 @@ public class TesteTipos {
     public static void main(String[] args) {
 
         //transforma de integer para string
-        int idade = 30;
+        int idade = 20;
         String valor = Integer.toString(idade);
         System.out.println(valor + " " + valor.getClass().getSimpleName());
 
         //transforma de string para integer
         String valor2 = "30";
-        int idade2 = Integer.parseInt(valor);
+        int idade2 = Integer.parseInt(valor2);
         System.out.println(idade2 + " " + ((Object)idade2).getClass().getSimpleName());
-
     }
 }
