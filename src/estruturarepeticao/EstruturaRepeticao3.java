519adcfaf8f27f5682bcde11de4ff594f935d880
renanpacheco21 <renan1025>
2022-07-05 21:42:38 -0300

    RenanCommit

---
+++ src/estruturarepeticao/EstruturaRepeticao3.java
@@ -0,0 +1,11 @@
+package estruturarepeticao;
+
+public class EstruturaRepeticao3 {
+
+    public static void main(String[] args) {
+
+        for (int num=0; num < 4; num++) {
+            System.out.println("Passou " + num);
+        }
+    }
+}
