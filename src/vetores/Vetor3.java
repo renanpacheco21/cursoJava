6fa0f44f57609c53de11aeef03563d78180816ec
renanpacheco21 <renan1025>
2022-07-11 20:31:58 -0300

    RenanCommit

--- src/vetores/Vetor3.java
+++ src/vetores/Vetor3.java
@@ -1,11 +1,15 @@
 package vetores;
 //exemplo de for each
 
+import java.util.Arrays;
+
 public class Vetor3 {
 
     public static void main(String[] args) {
 
-        double v[] = {3.5,4.8,5.8,6.8};
+        double v[] = {3.5,1.0,5.8,4.6,3.0,2.4};
+        Arrays.sort(v); //ordena o array
+
         for (double valor:v) {
             System.out.println("O valor é " + valor);
         }
---
+++ src/vetores/Vetor4.java
@@ -0,0 +1,19 @@
+package vetores;
+
+import java.util.Arrays;
+
+public class Vetor4 {
+
+    public static void main(String[] args) {
+
+        int num[] = {3, 5, 7, 2, 1, 9, 8};
+        for (int v:num
+             ) {
+            System.out.print(v + " ");
+        }
+        System.out.println("");
+        int p = Arrays.binarySearch(num,9);
+
+        System.out.println("Encontrei o valor na posição " + p);
+    }
+}
