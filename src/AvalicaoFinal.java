2b9a5e36a6a95d847cdc9d96ee7e7609434d41db
renanpacheco21 <renan1025>
2022-07-12 22:04:22 -0300

    RenanCommit

---
+++ src/AvalicaoFinal.java
@@ -0,0 +1,57 @@
+public class AvalicaoFinal {
+
+
+    public static void main(String[] args) {
+
+//        Object[] str = {"a", "ab", "abc"};
+//        System.out.println(str[1]);
+
+//        int total = 0;
+//        int c[] = new int[13];
+//        for (int i = 0; i<c.length; i++) {
+//            c[i] = i + i;
+//        }
+//        for (int i = 0; i<c.length; i++) {
+//            total += c[i];
+//        }
+//        System.out.println(total);
+
+//        for (int i = 1; i < 5; i++) {
+//            if (i==2) continue;
+//            System.out.print("i = " + i);
+//        }
+
+//        String nome = "João";
+//        imprimeNome("Empty");
+//    }
+//    public static void imprimeNome(String nome) {
+//        if(!nome.isEmpty()){
+//            System.out.println("Tudo bem " + nome + "?");
+//        } else {
+//            System.out.println("O nome é " + nome + "?");
+//        }
+//    }
+
+//        int v0 = 3;
+//        int v1 = v0++;
+//        int v2 = ++v1;
+//        v1 += v0;
+//        v2 += --v1;
+//
+//        System.out.println(v0);
+//        System.out.println(v1);
+//        System.out.println(v2);
+//
+//        double num = Math.pow(8,2);
+//        System.out.println(num);
+
+//        int x, y;
+//        double z;
+//
+//        x = 5;
+//        y = 2;
+//        z = x / y;
+//        System.out.println(z);
+
+    }
+}
